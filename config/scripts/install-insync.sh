cad7524a3fbce6dc2fdf1b1769869c7754a0104b
David McCrea <git@dmccrea.me>
2024-01-14 20:29:25 +0200

    install docker

--- config/scripts/install-insync.sh
+++ config/scripts/install-insync.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-# Tell this script to exit if there are any errors.
-# You should have this in every custom script, to ensure that your completed
-# builds actually ran successfully without any errors!
 set -oue pipefail
 
 rpm-ostree install https://cdn.insynchq.com/builds/linux/insync-3.8.7.50516-fc39.x86_64.rpm
